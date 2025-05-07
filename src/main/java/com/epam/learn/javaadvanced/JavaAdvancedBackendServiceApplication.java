daedb3c5a06a2fc05d89477014a45889ffdb4ec6
Siarhei Astapenka <siarhei_astapenka@epam.com>
2025-05-07 17:45:49 +0300

    Initial commit

---
+++ src/main/java/com/epam/learn/javaadvanced/JavaAdvancedBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.epam.learn.javaadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAdvancedBackendServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAdvancedBackendServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/learn/javaadvanced/JavaAdvancedBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.learn.javaadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAdvancedBackendServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
