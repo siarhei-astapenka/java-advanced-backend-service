ed6a906b24161509bc6745cfecf6b3811347f4ea
Siarhei Astapenka <siarhei_astapenka@epam.com>
2025-05-08 19:55:56 +0300

    Task 1 - Hello-world application

--- src/main/java/com/epam/learn/javaadvanced/JavaAdvancedBackendServiceApplication.java
+++ src/main/java/com/epam/learn/javaadvanced/JavaAdvancedBackendServiceApplication.java
@@ -1,13 +1,18 @@
 package com.epam.learn.javaadvanced;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaAdvancedBackendServiceApplication {
+public class JavaAdvancedBackendServiceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaAdvancedBackendServiceApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        System.out.println("Hello World!");
+    }
 }
